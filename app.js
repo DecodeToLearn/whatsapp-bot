f59b732dd3638f3ec76e38e18bc2f402a97bb73c
yusufsengul <arif_izzeti@outlook.com>
2025-01-07 18:34:19 +0300

    session add

--- app.js
+++ app.js
@@ -25,62 +25,113 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // WhatsApp Client
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu',
-        ],
-        defaultViewport: null,
-        timeout: 60000,
-    },
-});
+const SESSION_DIR = './sessions';  // Session klasörü
+if (!fs.existsSync(SESSION_DIR)) {
+    fs.mkdirSync(SESSION_DIR);
+}
 
-// Global Değişkenler
-let qrCodeUrl = '';
-let contacts = [];
+const clients = {};  // Kullanıcı bazlı client'lar
 
-// QR Kod Oluşturma ve Yayınlama
-client.on('qr', async (qr) => {
-    try {
-        qrCodeUrl = await qrcode.toDataURL(qr);
-        console.log('QR kodu oluşturuldu.');
-        broadcast({ type: 'qr', qrCode: qrCodeUrl });
-    } catch (error) {
-        console.error('QR kodu oluşturulurken hata:', error);
+function createClient(userId) {
+    const client = new Client({
+        authStrategy: new LocalAuth({
+            clientId: userId,
+            dataPath: SESSION_DIR,
+        }),
+        puppeteer: {
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu',
+            ],
+            defaultViewport: null,
+            timeout: 60000,
+        },
+    });
+
+    // QR Kod Oluşturma
+    client.on('qr', async (qr) => {
+        try {
+            const qrCodeUrl = await qrcode.toDataURL(qr);
+            console.log(`QR kodu (${userId}) oluşturuldu.`);
+            broadcast({ type: 'qr', qrCode: qrCodeUrl, userId });
+        } catch (error) {
+            console.error('QR kodu oluşturulurken hata:', error);
+        }
+    });
+
+    // Bağlantı Sağlandığında
+    client.on('ready', async () => {
+        console.log(`${userId} WhatsApp botu hazır.`);
+        try {
+            const contacts = (await client.getContacts()).map(contact => ({
+                id: contact.id._serialized,
+                name: contact.name || contact.pushname || contact.id.user,
+            }));
+            broadcast({ type: 'contacts', contacts, userId });
+        } catch (error) {
+            console.error('Kontaklar alınırken hata:', error);
+        }
+    });
+
+    // Bağlantı Kesildiğinde Yeniden Bağlanma
+    client.on('disconnected', (reason) => {
+        console.log(`${userId} bağlantısı kesildi: ${reason}`);
+        setTimeout(() => createClient(userId), 5000);  // 5 saniye sonra yeniden başlat
+    });
+
+    client.initialize();
+    clients[userId] = client;
+}
+
+// Kullanıcı Kaydı ve Client Oluşturma
+app.post('/register', (req, res) => {
+    const { userId } = req.body;
+
+    if (!userId) {
+        return res.status(400).json({ error: 'User ID gereklidir.' });
     }
-});
 
-// WhatsApp Bağlantısı Kurulduğunda
-client.on('ready', async () => {
-    console.log('WhatsApp botu hazır.');
-    try {
-        contacts = (await client.getContacts()).map(contact => ({
-            id: contact.id._serialized,
-            name: contact.name || contact.pushname || contact.id.user,
-        }));
-        broadcast({ type: 'contacts', contacts });
-    } catch (error) {
-        console.error('Kontaklar alınırken hata:', error);
+    if (clients[userId]) {
+        return res.json({ status: 'already_registered' });
     }
+
+    createClient(userId);
+    res.json({ status: 'registered' });
 });
 
-// WhatsApp Bağlantısı Kesildiğinde
-client.on('disconnected', async (reason) => {
-    console.log(`WhatsApp bağlantısı kesildi: ${reason}`);
-    try {
-        await client.destroy();
-        setTimeout(() => client.initialize(), 5000); // 5 saniye sonra yeniden başlat
-    } catch (error) {
-        console.error('Bağlantı yeniden başlatılamadı:', error);
+// QR Kod Endpoint
+app.get('/qr', (req, res) => {
+    if (qrCodeUrl) {
+        res.send(`
+            <html>
+            <head><title>WhatsApp QR Kodu</title></head>
+            <body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+                <div style="text-align: center;">
+                    <h1>WhatsApp QR Kodu</h1>
+                    <img src="${qrCodeUrl}" alt="WhatsApp QR" style="max-width: 100%; height: auto;" />
+                </div>
+            </body>
+            </html>
+        `);
+    } else {
+        res.send(`
+            <html>
+            <head><title>QR Kodu Oluşturuluyor...</title></head>
+            <body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+                <div style="text-align: center;">
+                    <h1>QR Kodu Oluşturuluyor</h1>
+                    <p>Lütfen bekleyin...</p>
+                </div>
+            </body>
+            </html>
+        `);
     }
 });
 
@@ -163,7 +214,7 @@ app.post('/send', async (req, res) => {
             const messageMedia = MessageMedia.fromFilePath(mediaPath);
             await client.sendMessage(formattedNumber, messageMedia, { caption });
 
-            fs.unlinkSync(mediaPath); // Geçici dosyayı sil
+            fs.unlinkSync(mediaPath);  // Geçici dosyayı sil
         } else if (caption) {
             await client.sendMessage(formattedNumber, caption);
         }
@@ -172,324 +223,6 @@ app.post('/send', async (req, res) => {
     } catch (error) {
         console.error('Mesaj gönderilirken hata oluştu:', error.message);
         res.status(500).json({ error: error.message });
-    }
-});
-
-/*app.post('/send', async (req, res) => {
-    const { number, caption, media } = req.body;
-
-    if (!number) {
-        return res.status(400).json({ error: 'Numara gereklidir.' });
-    }
-
-    try {
-        const formattedNumber = number.includes('@c.us') ? number : `${number}@c.us`;
-
-        if (media && media.url) {
-            const mediaContent = await MessageMedia.fromUrl(media.url);
-            await client.sendMessage(formattedNumber, mediaContent, { caption });
-            console.log('URL üzerinden medya gönderildi:', media.url);
-        } else if (caption) {
-            await client.sendMessage(formattedNumber, caption);
-            console.log('Metin mesajı gönderildi:', caption);
-        } else {
-            return res.status(400).json({ error: 'Mesaj veya medya bilgisi gereklidir.' });
-        }
-
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error('Mesaj gönderilirken hata oluştu:', error);
-        res.status(500).json({ error: error.message });
-    }
-});*/
-// Medya Dosyasını Geçici Bir Dizin'e Kaydetme
-const saveMediaToFile = (media) => {
-    if (!media || !media.data) {
-        console.error('Medya verisi eksik.');
-        return null;
-    }
-    const dir = path.join(__dirname, 'temp');
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-    const fileExtension = media.mimetype.split('/')[1] || 'bin';
-    const filePath = path.join(dir, `${Date.now()}-${Math.random().toString(36).substring(7)}.${fileExtension}`);
-    fs.writeFileSync(filePath, Buffer.from(media.data, 'base64'));
-    return filePath;
-};
-
-// URL'den Medya İndirme
-const downloadMedia = async (url) => {
-    try {
-        const response = await axios({
-            url,
-            method: 'GET',
-            responseType: 'arraybuffer',
-            timeout: 500000, // Zaman aşımı 10 saniye
-        });
-
-        const dir = path.join(__dirname, 'temp');
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-
-        const extension = path.extname(url).split('?')[0] || '.bin';
-        const filePath = path.join(dir, `${Date.now()}-${Math.random().toString(36).substring(7)}${extension}`);
-        fs.writeFileSync(filePath, response.data);
-        return filePath;
-    } catch (error) {
-        console.error('Medya indirilemedi:', error.message);
-        return null;
-    }
-};
-//Group Operations
-app.post('/create-group', async (req, res) => {
-    const { groupName, contacts } = req.body;
-
-    if (!groupName || !contacts || !contacts.length) {
-        return res.status(400).json({ error: 'Grup adı ve kişiler gerekli.' });
-    }
-
-    try {
-        const result = await client.createGroup(groupName, contacts.map(number => `${number}@c.us`));
-        res.status(200).json({ success: true, groupId: result.gid._serialized });
-    } catch (error) {
-        console.error('Grup oluşturulurken hata:', error.message);
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get('/groups', async (req, res) => {
-    try {
-        const contacts = await client.getContacts(); // Tüm kişiler
-        const groups = contacts
-            .filter(contact => contact.id._serialized.includes('@g.us')) // ID'de '@g.us' olanları filtrele
-            .map(contact => ({
-                id: contact.id._serialized, // Grup ID'si
-                name: contact.name || 'Unknown Group' // Grup adı veya varsayılan ad
-            }));
-
-        if (groups.length === 0) {
-            return res.status(200).json({ message: 'Grup bulunamadı.' });
-        }
-
-        res.status(200).json({ groups });
-    } catch (error) {
-        console.error('Gruplar alınırken hata:', error.message);
-        res.status(500).json({ error: `Sunucu hatası: ${error.message}` });
-    }
-});
-
-//********** Broadcast Start */
-// Broadcast Oluşturma
-/*app.post('/create-broadcast', async (req, res) => {
-    const { broadcastName, contacts } = req.body;
-
-    if (!broadcastName || !contacts || !contacts.length) {
-        return res.status(400).json({ error: 'Broadcast adı ve kişiler gerekli.' });
-    }
-
-    try {
-        const chatIds = contacts.map(number => `${number}@c.us`);
-        const broadcastList = {
-            id: Date.now().toString(),
-            name: broadcastName,
-            contacts: chatIds
-        };
-
-        const filePath = path.join(__dirname, 'broadcasts.json');
-        let broadcastData = [];
-        if (fs.existsSync(filePath)) {
-            broadcastData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-        }
-        broadcastData.push(broadcastList);
-        fs.writeFileSync(filePath, JSON.stringify(broadcastData, null, 2));
-
-        res.status(200).json({ success: true, broadcastId: broadcastList.id });
-    } catch (error) {
-        console.error('Broadcast oluşturulurken hata:', error.message);
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// Broadcast Listesi Getirme
-/*app.get('/broadcasts', async (req, res) => {
-    try {
-        const chats = await client.getChats();
-        if (!chats) {
-            return res.status(404).json({ error: 'Chat listesi bulunamadı.' });
-        }
-
-        const broadcasts = chats
-            .filter(chat => chat.isBroadcast)
-            .map(broadcast => ({
-                id: broadcast.id._serialized,
-                name: broadcast.name || 'Broadcast List'
-            }));
-        
-        if (broadcasts.length === 0) {
-            return res.status(404).json({ error: 'Mevcut broadcast bulunamadı.' });
-        }
-
-        res.status(200).json({ broadcasts });
-    } catch (error) {
-        console.error('Broadcast listeleri alınırken hata:', error.message);
-        res.status(500).json({ error: 'Sunucu hatası: ' + error.message });
-    }
-});
-
-app.get('/broadcasts', async (req, res) => {
-    try {
-        const contacts = await client.getContacts(); // Tüm kişiler
-        const broadcasts = contacts
-            .filter(contact => contact.name && contact.name.includes('toplu')) // 'toplu' kelimesini içerenleri filtrele
-            .map(contact => ({
-                id: contact.id._serialized, // Broadcast ID
-                name: contact.name // Broadcast Adı
-            }));
-
-        if (broadcasts.length === 0) {
-            return res.status(200).json({ message: 'Broadcast bulunamadı.' });
-        }
-
-        res.status(200).json({ broadcasts });
-    } catch (error) {
-        console.error('Broadcast listesi alınırken hata:', error.message);
-        res.status(500).json({ error: `Sunucu hatası: ${error.message}` });
-    }
-});
-
-
-
-// Broadcast Mesaj Gönderme
-app.post('/send-broadcast-message', async (req, res) => {
-    const { broadcastId, message } = req.body;
-
-    if (!broadcastId || !message) {
-        return res.status(400).json({ error: 'Broadcast ID ve mesaj gerekli.' });
-    }
-
-    try {
-        await client.sendMessage(broadcastId, message);
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error('Broadcast mesaj gönderilirken hata:', error.message);
-        res.status(500).json({ error: error.message });
-    }
-});
-
-
-
-app.post('/send-message-by-chat-name', async (req, res) => {
-    const { chatName, message } = req.body;
-
-    if (!chatName || !message) {
-        return res.status(400).json({ error: 'Chat adı ve mesaj gereklidir.' });
-    }
-
-    try {
-        // Tüm sohbetleri al
-        const chats = await client.getChats();
-
-        // Chat adı eşleşen sohbeti bul
-        const targetChat = chats.find(chat => chat.name && chat.name === chatName);
-
-        if (!targetChat) {
-            return res.status(404).json({ error: `Chat '${chatName}' bulunamadı.` });
-        }
-
-        // Mesaj gönder
-        await client.sendMessage(targetChat.id._serialized, message);
-        res.status(200).json({ success: true, chatId: targetChat.id._serialized });
-    } catch (error) {
-        console.error('Mesaj gönderilirken hata:', error.message);
-        res.status(500).json({ error: `Sunucu hatası: ${error.message}` });
-    }
-});
-app.post('/send-test-message', async (req, res) => {
-    const { chatId, message } = req.body;
-
-    if (!chatId || !message) {
-        return res.status(400).json({ error: 'Chat ID ve mesaj gereklidir.' });
-    }
-
-    try {
-        await client.sendMessage(chatId, message);
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error('Test mesaj gönderilirken hata:', error.message);
-        res.status(500).json({ error: error.message });
-    }
-});
-*/
-//***********Broadcast End */
-
-app.post('/send-group-media', async (req, res) => {
-    const { groupId, caption, mediaUrl } = req.body;
-
-    if (!groupId || !mediaUrl) {
-        return res.status(400).json({ error: 'Grup ID ve medya URL gereklidir.' });
-    }
-
-    try {
-        // Gruba mesaj göndermek için ID'nin formatını kontrol et
-        const formattedGroupId = groupId.includes('@g.us') ? groupId : `${groupId}@g.us`;
-
-        // Medya dosyasını oluştur
-        const media = await MessageMedia.fromUrl(mediaUrl);
-
-        // Mesajı gruba gönder
-        await client.sendMessage(formattedGroupId, media, { caption });
-
-        res.status(200).json({ success: true, message: 'Medya mesajı başarıyla gönderildi.' });
-    } catch (error) {
-        console.error('Medya mesajı gönderilirken hata:', error.message);
-        res.status(500).json({ error: `Sunucu hatası: ${error.message}` });
-    }
-});
-
-
-app.post('/send-group-message', async (req, res) => {
-    const { groupId, message } = req.body;
-
-    if (!groupId || !message) {
-        return res.status(400).json({ error: 'Grup ID ve mesaj gerekli.' });
-    }
-
-    try {
-        await client.sendMessage(groupId, message);
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error('Mesaj gönderilirken hata:', error.message);
-        res.status(500).json({ error: error.message });
-    }
-});
-// QR Kod Endpoint
-app.get('/qr', (req, res) => {
-    if (qrCodeUrl) {
-        res.send(`
-            <html>
-            <head><title>WhatsApp QR Kodu</title></head>
-            <body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-                <div style="text-align: center;">
-                    <h1>WhatsApp QR Kodu</h1>
-                    <img src="${qrCodeUrl}" alt="WhatsApp QR" style="max-width: 100%; height: auto;" />
-                </div>
-            </body>
-            </html>
-        `);
-    } else {
-        res.send(`
-            <html>
-            <head><title>QR Kodu Oluşturuluyor...</title></head>
-            <body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-                <div style="text-align: center;">
-                    <h1>QR Kodu Oluşturuluyor</h1>
-                    <p>Lütfen bekleyin...</p>
-                </div>
-            </body>
-            </html>
-        `);
     }
 });
 
