ace471d215bd4bdbd28e324b7ffae251bd806a61
yusufsengul <arif_izzeti@outlook.com>
2025-01-30 17:18:04 +0300

    add clients to app

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const WebSocket = require('ws');
 const path = require('path');
 const app = express();
 const axios = require('axios');
+
+const { clients } = require('./instagram'); 
+
 app.use('/media', express.static(path.join(__dirname, 'media')));
 const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
